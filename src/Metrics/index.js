8a26a68177b741332b60a51ae5a46d46e0cf5f97
Jonathan Hurter <john@scalingo.com>
2019-01-18 18:59:07 +0100

    Fix typo in doc

--- src/Metrics/index.js
+++ src/Metrics/index.js
@@ -26,7 +26,7 @@ export default class Metrics {
    * @param {Object} opts Optional params
    * @param {Number} opts.since Number of hours requested (max 71)
    * @param {String} opts.statusCode Only available when requesting router metrics. One of the following: [all, 1XX, 2XX, 3XX, 4XX, 5XX]
-   * @param {String} opts.statisticType Only available when requesting requests metrics. One of the following: [media, p95, p99]
+   * @param {String} opts.statisticsType Only available when requesting requests metrics. One of the following: [media, p95, p99]
    * @param {String} opts.containerType Container type filter
    * @param {Number} opts.containerIndex Container index (only available when containerType is set and for non router metrics)
    * @param {Boolean} opts.last only fetch the last metrics
