f0a4e97fecfed3af208ae2026f05b50cf0b32631
Jonathan Hurter <john@scalingo.com>
2019-05-07 15:52:09 +0200

    Add App#create

---
+++ examples/Apps/create.js
@@ -0,0 +1,7 @@
+let scalingo = require('../../dist/scalingo.js');
+
+scalingo.clientFromToken(process.env.SCALINGO_TOKEN).then((client)=>{
+  return client.Apps.create("john-test-scalingojs", {dry_run: true})
+}).then((app) => {
+  console.log(app)
+})
--- src/Apps/index.js
+++ src/Apps/index.js
@@ -30,6 +30,32 @@ export default class Apps{
   all() {
     return unpackData(this._client.apiClient().get('/apps'), "apps")
   }
+
+  /**
+   * Create a new application
+   * @see https://developers.scalingo.com/apps#create-an-application
+   * @param {String} name - Name of the application
+   * @param {AppCreateOpts} opts - Optionnal additionnal information
+   * @return {Promise<App, APIError>} Promise that when resolved return an the App created. See:
+   */
+
+  create(name, opts) {
+    let body = {
+      name: name,
+    }
+
+    let headers = {}
+
+    if(opts) {
+      body['git_source'] = opts['git_source']
+      body['parent_id'] = opts['parent_id']
+      body['stack_id'] = opts['stack_id']
+      if(opts['dry_run']) {
+        headers['X-Dry-Run'] = "true"
+      }
+    }
+    return unpackData(this._client.apiClient().post('/apps', {app: body}, {headers}), "app")
+  }
 }
 
 
@@ -50,4 +76,12 @@ export default class Apps{
  * @property {Date} last_deployed_at date of the last deployment attempt
  * @property {String} last_deployed_by user who attempted the last deployment
  * @property {String} last_deployment_id id of the last successful deployment
+ */
+
+/**
+ * @typedef {Object} AppCreateOpts
+ * @see https://developers.scalingo.com/apps#create-an-application
+ * @property {String} parent_id ID of the parent app (used to create child apps)
+ * @property {String} stack_id ID of the stack the application should use
+ * @property {Boolean} dry_run If set to true, the API will run the validations but wont create the app
  */
