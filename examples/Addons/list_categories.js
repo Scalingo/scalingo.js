f6c8efc8136927afc65ff1ac720b3e205bd91280
Valentin ICHKOUR <valentin.ichkour@epitech.eu>
2019-06-03 14:34:06 +0200

    methods => create - list categories - list providers

--- examples/Addons/list_categories.js
+++ examples/Addons/list_categories.js
@@ -1,7 +1,7 @@
 let scalingo = require('../../dist/scalingo.js');
 
 scalingo.clientFromToken(process.env.SCALINGO_TOKEN).then((client)=>{
-  return client.Addons.for("test-dashboard-ichkour")
-}).then((addons) => {
-  console.log(addons)
+  return client.Addons.listCategories()
+}).then((categories) => {
+  console.log(categories)
 });
