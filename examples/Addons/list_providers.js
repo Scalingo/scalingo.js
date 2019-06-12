9da48d2a0030fcb8aa8a080916df9405b3ea5d76
Valentin Ichkour <valentin.ichkour@epitech.eu>
2019-06-12 15:22:51 +0200

    fix listProviders method

--- examples/Addons/list_providers.js
+++ examples/Addons/list_providers.js
@@ -2,7 +2,7 @@ let scalingo = require('../../dist/scalingo.js');
 
 // For database id : 54c6909b61646d0001000000
 scalingo.clientFromToken(process.env.SCALINGO_TOKEN).then((client)=>{
-  return client.Addons.listProviders("1234")
+  return client.Addons.listProviders()
 }).then((providers) => {
   console.log(providers)
 }).catch(e => {
