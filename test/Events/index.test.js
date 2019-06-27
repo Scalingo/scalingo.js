aad6684f0e5d039e981361737fe01cdb5892ef12
Valentin Ichkour <valentin.ichkour@epitech.eu>
2019-06-27 13:53:48 +0200

    unit tests

---
+++ test/Events/index.test.js
@@ -0,0 +1,11 @@
+import {testGetter} from '../utils/http.js'
+import Events from '../../src/Events'
+
+describe("Events#for", () =>  {
+  testGetter("https://api.scalingo.com/v1/apps/tata/events?from=2", null, null, (client) => {
+    return new Events(client).for("tata", 2)
+  })
+  testGetter("https://api.scalingo.com/v1/apps/tata/events", null, null, (client) => {
+    return new Events(client).for("tata")
+  })
+})
