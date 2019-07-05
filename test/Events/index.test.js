c20760caf0fd516610fef922f7f86a7d1561ea6d
Valentin Ichkour <valentin.ichkour@epitech.eu>
2019-07-05 17:00:36 +0200

    remove old unit tests

--- test/Events/index.test.js
+++ test/Events/index.test.js
@@ -2,20 +2,8 @@ import {testGetter} from '../utils/http.js'
 import Events from '../../src/Events'
 
 describe("Events#for", () =>  {
-  testGetter("https://api.scalingo.com/v1/apps/tata/events?from=2&page=1&per_page=10", null, null, (client) => {
-    return new Events(client).for("tata", 2, {page: 1, per_page: 10})
-  })
-  testGetter("https://api.scalingo.com/v1/apps/tata/events?page=4&per_page=20", null, null, (client) => {
-    return new Events(client).for("tata", null, {page: 4})
-  })
   testGetter("https://api.scalingo.com/v1/apps/tata/events", null, null, (client) => {
-    return new Events(client).for("tata")
-  })
-})
-
-describe("Events#forUser", () => {
-  testGetter("https://api.scalingo.com/v1/events", null, null, (client) => {
-    return new Events(client).forUser()
+    return new Events(client).for("tata", {page: 1, from: 2})
   })
 })
 
