39261db72456db43ae647b7629c5ea8a33c96bb1
Kevin Soltysiak <k@ksol.fr>
2019-12-08 13:12:29 +0100

    Tokens: rename example too

--- examples/Tokens/all.js
+++ examples/Tokens/all.js
@@ -3,7 +3,7 @@ const scalingo = require('../../dist/scalingo.js')
 scalingo
   .clientFromToken(process.env.SCALINGO_TOKEN)
   .then((client) => {
-    return client.Tokens.list()
+    return client.Tokens.all()
   })
   .then((tokens) => {
     console.log(tokens)
