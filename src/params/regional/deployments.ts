b6804f2c304ea5659cbb8fd8b3d0aebbe519ffb2
Kevin Soltysiak <kevin@scalingo.com>
2021-07-15 16:21:15 +0200

    feat: deployments, create from archive

---
+++ src/params/regional/deployments.ts
@@ -0,0 +1,8 @@
+export interface CreateParams {
+  /** URL of the archive to deploy */
+  source_url: string;
+  /** String identifying the archive */
+  git_ref?: string;
+  /** A command to be executed after the deployment and before the app starts */
+  postdeploy_hook?: string;
+}
