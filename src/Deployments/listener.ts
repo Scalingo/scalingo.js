15375ff73e9e1a54510ba06643b7d4272ec147bb
Kevin Soltysiak <kevin@scalingo.com>
2021-12-21 14:53:38 +0100

    feat: bump ws dependency

--- src/Deployments/listener.ts
+++ src/Deployments/listener.ts
@@ -42,7 +42,7 @@ export interface MessageHandlers {
 
 export interface LifecycleHandlers {
   beforeOpen: (() => void)[];
-  onOpen: ((e?: WebSocket.OpenEvent) => void)[];
+  onOpen: ((e?: WebSocket.Event) => void)[];
   beforeClose: (() => void)[];
   onClose: ((e?: WebSocket.CloseEvent) => void)[];
 }
@@ -177,7 +177,7 @@ export default class Listener {
    * Setup a handler that will be called when the connection is opened.
    * @param handler handler to call
    */
-  onOpen(handler: (e?: WebSocket.OpenEvent) => void): void {
+  onOpen(handler: (e?: WebSocket.Event) => void): void {
     this.lifecycleHandlers.onOpen.push(handler);
   }
 
