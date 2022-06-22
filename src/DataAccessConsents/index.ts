f6465031bfba75251ecd7c9f3b69a8ac9fcc50a4
Anouchka-M <anouchka@scalingo.com>
2022-06-22 15:58:37 +0200

    style: linter DataAccessConsent

--- src/DataAccessConsents/index.ts
+++ src/DataAccessConsents/index.ts
@@ -29,14 +29,25 @@ export default class DataAccessConsents {
     );
   }
 
-  /** 
+  /**
    * Create an agreement for the given contract
    * @see https://developers.scalingo.com/data_access_consents#create-a-new-dataaccessconsent
    * @param appId ID of the app to get data_access_consents list
-  */
-  createDataAccessConsent(appId: string, end_at: string, databases: boolean, containers: boolean): Promise<DataAccessConsent> {
+   */
+  createDataAccessConsent(
+    appId: string,
+    end_at: string,
+    databases: boolean,
+    containers: boolean
+  ): Promise<DataAccessConsent> {
     return unpackData(
-      this._client.apiClient().post(`/apps/${appId}/data_access_consents`,  { data_access_consent: { end_at: end_at, databases: databases, containers: containers } }),
+      this._client.apiClient().post(`/apps/${appId}/data_access_consents`, {
+        data_access_consent: {
+          end_at: end_at,
+          databases: databases,
+          containers: containers,
+        },
+      }),
       "data_access_consent"
     );
   }
