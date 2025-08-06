b680ecf7a597bf44c849a37640a0b0fd5bc820a8
Baptiste MAURY <baptiste.maury@scalingo.com>
2025-08-06 09:48:04 +0200

    Move create into index

--- src/Databases/index.ts
+++ src/Databases/index.ts
@@ -29,4 +29,27 @@ export default class Databases {
       "apps",
     );
   }
+
+  /**
+   * Create a new database
+   * @see https://developers.scalingo.com/databases#provision-a-database
+   * @param opts Object with necessary and optionnal params
+   * @param opts.addon_provider_id ID of the addon provider
+   * @param opts.plan_id ID of the plan
+   * @param opts.database_name Name of the database
+   *
+   * @return Promise that when resolved returns the new database.
+   */
+
+  create(opts: {
+    addon_provider_id: string;
+    plan_id: string;
+    database_name: string;
+    project_id?: string;
+  }): Promise<Database[] | DashboardDatabase[]> {
+    return unpackData(
+      this._client.apiClient().post("/databases", { params: opts }),
+      "apps",
+    );
+  }
 }
