42272a94431d5c8b345dcb36ba7038bcd5166edf
Baptiste MAURY <baptiste.maury@scalingo.com>
2025-08-06 12:13:31 +0200

    Update create part

--- src/Databases/index.ts
+++ src/Databases/index.ts
@@ -1,5 +1,5 @@
 import { Client } from "..";
-import { Database, DashboardDatabase } from "../models/regional/databases";
+import { Database, DashboardDatabase, CreateParams } from "../models/regional/databases";
 import { unpackData } from "../utils";
 
 /**
@@ -32,23 +32,18 @@ export default class Databases {
 
   /**
    * Create a new database
-   * @see https://developers.scalingo.com/databases#provision-a-database
-   * @param opts Object with necessary and optionnal params
-   * @param opts.addon_provider_id ID of the addon provider
-   * @param opts.plan_id ID of the plan
-   * @param opts.database_name Name of the database
+   * @param addon_provider_id ID of the addon provider
+   * @param plan_id ID of the plan
+   * @param database_name Name of the database
    *
    * @return Promise that when resolved returns the new database.
    */
 
-  create(opts: {
-    addon_provider_id: string;
-    plan_id: string;
-    database_name: string;
-    project_id?: string;
-  }): Promise<Database[] | DashboardDatabase[]> {
+  create(
+    createParams: CreateParams,
+  ): Promise<Database[] | DashboardDatabase[]> {
     return unpackData(
-      this._client.apiClient().post("/databases", { opts }),
+      this._client.apiClient().post("/databases", { database: createParams }),
       "apps",
     );
   }
